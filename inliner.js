12437f0bfdf6e4869b1c87b19d267a3e47ffd6ad
mfrancis95 <mikefrancis95@gmail.com>
2015-12-26 18:10:21 -0500

    Added callback functionality. Inlinifies JavaScript as well.

--- inliner.js
+++ inliner.js
@@ -1,21 +1,48 @@
 var cheerio = require("cheerio");
 var fs = require("fs");
 
-function inlinifyCSS(inputFile, outputFile) {
+function inlinify(inputFile, outputFile, properties) {
+    properties = properties || {};
+    var callback = properties.callback;
+    var css = properties.css || true;
+    var js = properties.js || true;
     fs.readFile(inputFile, function(error, data) {
+        var elements = 0, elementsDone = 0;
         var $ = cheerio.load(data.toString());
-        var head = $("head");
-        $("link[rel=stylesheet]").each(function() {
-            var link = $(this);
-            var href = link.attr("href");
-            if (href.slice(0, 4) !== "http") {
-                fs.readFile(href, function(error, data) {
-                    var css = data.toString();
-                    link.remove();
-                    head.append("<style>" + css + "</style>");
-                    fs.writeFile(outputFile, $.html());
+        if (css) {
+            var head = $("head");
+            var links = head.children("link[rel=stylesheet]");
+            elements += links.length;
+            links.each(function () {
+                var link = $(this);
+                fs.readFile(link.attr("href"), function(error, data) {
+                    if (!error) {
+                        link.remove();
+                        head.append("<style>" + data.toString() + "</style>");
+                    }
+                    if (++elementsDone === elements) {
+                        fs.writeFile(outputFile, $.html(), callback);
+                    }
                 });
-            }
-        });
+            });
+        }
+        if (js) {
+            var scripts = $("script");
+            elements += scripts.length;
+            scripts.each(function() {
+                var script = $(this);
+                fs.readFile(script.attr("src"), function(error, data) {
+                    if (!error) {
+                        script.removeAttr("src").text(data.toString());
+                    }
+                    if (++elementsDone === elements) {
+                        fs.writeFile(outputFile, $.html(), callback);
+                    }
+                });
+            });
+        }
+        if (elements === 0 && callback) {
+            callback();
+        }
     });
 }
