af4302a2342ff6ea62067293604a336d0c2b655b
mfrancis95 <mikefrancis95@gmail.com>
2016-02-14 17:35:02 -0500

    Use Promises

--- inlinify.js
+++ inlinify.js
@@ -1,54 +1,54 @@
 var cheerio = require("cheerio");
 var fs = require("fs");
 
-function inlinifyCSS(html, callback) {
+function inlinifyCSS(html) {
     var elements = 0, elementsDone = 0;
     var $ = cheerio.load(html);
     var head = $("head");
     var links = head.children("link[rel=stylesheet]");
     elements += links.length;
-    if (elements === 0) {
-        if (callback) {
-            callback(html);
-        }
-    }
-    else {
-        links.each(function() {
-            var link = $(this);
-            fs.readFile(link.attr("href"), (error, data) => {
-                if (!error) {
-                    link.remove();
-                    head.append("<style>" + data.toString() + "</style>");
-                }
-                if (++elementsDone === elements && callback) {
-                    callback($.html());
-                }
+    return new Promise(resolve => {   
+        if (elements === 0) {
+            resolve(html);
+        }          
+        else {
+            links.each(function() {
+                var link = $(this);
+                fs.readFile(link.attr("href"), (error, data) => {
+                    if (!error) {
+                        link.remove();
+                        head.append("<style>" + data.toString() + "</style>");
+                    }
+                    if (++elementsDone === elements) {
+                        resolve($.html());
+                    }
+                });
             });
-        });
-    }
+        }
+    });
 }
 
-function inlinifyJS(html, callback) {
+function inlinifyJS(html) {
     var elements = 0, elementsDone = 0;
     var $ = cheerio.load(html);
     var scripts = $("script");
     elements += scripts.length;
-    if (elements === 0) {
-        if (callback) {
-            callback(html);
-        }
-    }
-    else {
-        scripts.each(function() {
-            var script = $(this);
-            fs.readFile(script.attr("src"), (error, data) => {
-                if (!error) {
-                    script.removeAttr("src").text(data.toString());
-                }
-                if (++elementsDone === elements && callback) {
-                    callback($.html());
-                }
+    return new Promise(resolve => {    
+        if (elements === 0) {
+            resolve(html);
+        }    
+        else {
+            scripts.each(function() {
+                var script = $(this);
+                fs.readFile(script.attr("src"), (error, data) => {
+                    if (!error) {
+                        script.removeAttr("src").text(data.toString());
+                    }
+                    if (++elementsDone === elements) {
+                        resolve($.html());
+                    }
+                });
             });
-        });
-    }
+        }
+    });
 }
