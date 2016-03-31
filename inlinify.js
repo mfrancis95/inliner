f6372c819958bcb6f6368332d22fc4807d2ca3f2
mfrancis95 <mikefrancis95@gmail.com>
2016-03-31 09:21:40 -0400

    Remove async and defer attributes from inlinified scripts

--- inlinify.js
+++ inlinify.js
@@ -2,22 +2,22 @@ var cheerio = require("cheerio");
 var fs = require("fs");
 
 function inlinifyCSS(html) {
-    var elements = 0, elementsDone = 0;
-    var $ = cheerio.load(html);
-    var head = $("head");
-    var links = head.children("link[rel=stylesheet]");
-    elements += links.length;
-    return new Promise(resolve => {   
+    return new Promise(resolve => {
+        var elementsDone = 0;
+        var $ = cheerio.load(html);
+        var head = $("head");
+        var links = head.children("link[rel=stylesheet]");
+        var elements = links.length;
         if (elements === 0) {
             resolve(html);
-        }          
+        }
         else {
             links.each(function() {
                 var link = $(this);
                 fs.readFile(link.attr("href"), (error, data) => {
                     if (!error) {
                         link.remove();
-                        head.append(`<style>${data.toString()}</style>`);
+                        head.append(`<style>${data.toString().replace(/\.\.\//g, "")}</style>`);
                     }
                     if (++elementsDone === elements) {
                         resolve($.html());
@@ -29,20 +29,24 @@ function inlinifyCSS(html) {
 }
 
 function inlinifyJS(html) {
-    var elements = 0, elementsDone = 0;
-    var $ = cheerio.load(html);
-    var scripts = $("script");
-    elements += scripts.length;
-    return new Promise(resolve => {    
+    return new Promise(resolve => {
+        var elementsDone = 0;
+        var $ = cheerio.load(html);
+        var scripts = $("script");
+        var elements = scripts.length;
         if (elements === 0) {
             resolve(html);
-        }    
+        }
         else {
+            var body = $("body");
             scripts.each(function() {
                 var script = $(this);
                 fs.readFile(script.attr("src"), (error, data) => {
                     if (!error) {
-                        script.removeAttr("src").text(data.toString());
+                        script.removeAttr("async").removeAttr("src").text(data.toString());
+                        if (script.attr("defer")) {
+                            body.append(script.removeAttr("defer").remove());
+                        }
                     }
                     if (++elementsDone === elements) {
                         resolve($.html());
@@ -53,5 +57,5 @@ function inlinifyJS(html) {
     });
 }
 
-module.exports.css = inlinifyCSS;
-module.exports.js = inlinifyJS;
+exports.css = inlinifyCSS;
+exports.js = inlinifyJS;
